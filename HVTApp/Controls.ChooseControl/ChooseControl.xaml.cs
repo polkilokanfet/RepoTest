255b1a880a0ac133c474619e9c1ba9e85f34f2d7
kosolapov (house) <kosolapov.ag@gmail.com>
2017-03-30 02:59:44 +0500

    ChooseControl

---
+++ HVTApp/Controls.ChooseControl/ChooseControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Controls.ChooseControl
+{
+    /// <summary>
+    /// Interaction logic for ChooseControl.xaml
+    /// </summary>
+    public partial class ChooseControl : UserControl
+    {
+        public ChooseControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ItemProperty = DependencyProperty.Register(
+            "Item", typeof(object), typeof(ChooseControl), new PropertyMetadata(default(object)));
+
+        public object Item
+        {
+            get { return (object) GetValue(ItemProperty); }
+            set { SetValue(ItemProperty, value); }
+        }
+
+        public static readonly RoutedEvent ItemChangedEvent = EventManager.RegisterRoutedEvent(
+            nameof(ItemChanged), RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<object>), typeof(ChooseControl));
+
+        public event RoutedPropertyChangedEventHandler<Color> ItemChanged
+        {
+            add { AddHandler(ItemChangedEvent, value); }
+            remove { RemoveHandler(ItemChangedEvent, value); }
+        }
+    }
+}
