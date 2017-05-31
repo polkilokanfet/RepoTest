5f680b2fa8e753bf22af68b79eed4ed2bc2fd10b
ALex Kos <kosolapov.ag@gmail.com>
2017-05-31 17:42:58 +0500

    31.05.17 2

--- HVTApp/Modules/HVTApp.Modules.Sales/ViewModels/MarketViewModel.cs
+++ HVTApp/Modules/HVTApp.Modules.Sales/ViewModels/MarketViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace HVTApp.Modules.Sales.ViewModels
+﻿using System.Collections.ObjectModel;
+using HVTApp.DataAccess;
+using HVTApp.Model.Wrappers;
+
+namespace HVTApp.Modules.Sales.ViewModels
 {
     public class MarketViewModel
     {
+        public ObservableCollection<ProjectWrapper> Projects { get; set; }
+        public MarketViewModel(IUnitOfWork unitOfWork)
+        {
+            Projects = new ObservableCollection<ProjectWrapper>(unitOfWork.Projects.GetAll());
+        }
     }
 }
--- HVTApp/Modules/HVTApp.Modules.Sales/Views/ProjectsView.xaml.cs
+++ HVTApp/Modules/HVTApp.Modules.Sales/Views/ProjectsView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using HVTApp.Infrastructure;
+﻿using System.Collections;
+using System.Windows;
+using HVTApp.Infrastructure;
 using HVTApp.Modules.Infrastructure;
 using Prism.Events;
 using Prism.Regions;
@@ -11,6 +13,19 @@ namespace HVTApp.Modules.Sales.Views
         public ProjectsView(IRegionManager regionManager, IEventAggregator eventAggregator) : base(regionManager, eventAggregator)
         {
             InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ProjectsProperty = DependencyProperty.Register(
+            "Projects", typeof (IEnumerable), typeof (ProjectsView), new PropertyMetadata(default(IEnumerable)));
+
+        public IEnumerable Projects
+        {
+            get { return (IEnumerable) GetValue(ProjectsProperty); }
+            set
+            {
+                SetValue(ProjectsProperty, value);
+                ProjectsDataGrid.DataSource = value;
+            }
         }
     }
 }
