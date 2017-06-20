71572ec9118a993bdc0ff075832ffba44468693f
ALex Kos <kosolapov.ag@gmail.com>
2017-06-20 17:56:59 +0500

    20.06.17

--- HVTApp/HVTApp.Model.Tests/ProductTests.cs
+++ HVTApp/HVTApp.Model.Tests/ProductTests.cs
@@ -9,13 +9,45 @@ namespace HVTApp.Model.Tests
     [TestClass]
     public class ProductTests
     {
+
+        private Parameter _parameter1, _parameter2, _parameter3, _parameter4, _parameter5, _parameter6, _parameter7;
+        private ProductItem _productItem1, _productItem2, _productItem3, _productItem4;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _parameter1 = new Parameter { Id = 1, Value = "parameter1" };
+            _parameter2 = new Parameter { Id = 2, Value = "parameter2" };
+            _parameter3 = new Parameter { Id = 3, Value = "parameter3" };
+            _parameter4 = new Parameter { Id = 4, Value = "parameter4" };
+            _parameter5 = new Parameter { Id = 5, Value = "parameter5" };
+            _parameter6 = new Parameter { Id = 6, Value = "parameter6" };
+            _parameter7 = new Parameter { Id = 7, Value = "parameter7" };
+
+            _productItem1 = new ProductItem { Id = 1, Designation = "ProductItem1", Parameters = new List<Parameter> { _parameter1, _parameter2 } };
+            _productItem2 = new ProductItem { Id = 2, Designation = "ProductItem2", Parameters = new List<Parameter> { _parameter3, _parameter4 } };
+            _productItem3 = new ProductItem { Id = 3, Designation = "ProductItem3", Parameters = new List<Parameter> { _parameter5, _parameter6 } };
+            _productItem4 = new ProductItem { Id = 4, Designation = "ProductItem3", Parameters = new List<Parameter> { _parameter7 } };
+        }
+
+        [TestMethod]
+        public void Product_EqualsWorks()
+        {
+            Product product1 = new Product { ProductItem = _productItem1 };
+            Product product2 = new Product { ProductItem = _productItem2 };
+            Product product3 = new Product { ProductItem = _productItem3, ChildProducts = new List<Product> {product1, product2} };
+            Product product4 = new Product { ProductItem = _productItem3, ChildProducts = new List<Product> {product2, product1} };
+
+            Assert.IsTrue(Equals(product3, product4));
+        }
+
         [TestMethod]
         public void ProductTotalPriceTest()
         {
             SumOnDate price1 = new SumOnDate { Sum = 10, Date = DateTime.Today.AddDays(-7) };
             SumOnDate price2 = new SumOnDate { Sum = 5, Date = DateTime.Today };
 
-            var productParent = WrappersFactory.GetWrapper<Product, ProductWrapper>(new Product {ProductItem = new ProductItem()});
+            var productParent = WrappersFactory.GetWrapper<Product, ProductWrapper>(new Product {ProductItem = _productItem1});
             productParent.ProductItem.Prices.Add(WrappersFactory.GetWrapper <SumOnDate, SumOnDateWrapper> (price1));
             productParent.ProductItem.Prices.Add(WrappersFactory.GetWrapper <SumOnDate, SumOnDateWrapper> (price2));
             productParent.TotalPriceDate = DateTime.Today;
@@ -50,25 +82,17 @@ namespace HVTApp.Model.Tests
         [TestMethod]
         public void ProductSameParametersTest()
         {
-            Parameter p1 = new Parameter { Value = "p1p" };
-            Parameter p2 = new Parameter { Value = "p2p" };
+            ProductItemWrapper productItemWrapper1 = WrappersFactory.GetWrapper <ProductItem, ProductItemWrapper> (_productItem1);
+            ProductItemWrapper productItemWrapper2 = WrappersFactory.GetWrapper <ProductItem, ProductItemWrapper> (_productItem2);
 
-            ProductItem pr1 = new ProductItem();
-            pr1.Parameters.Add(p1);
-            pr1.Parameters.Add(p2);
-            ProductItem pr2 = new ProductItem();
+            Assert.IsFalse(productItemWrapper1.HasSameParameters(productItemWrapper2));
 
-            ProductItemWrapper pr1W = WrappersFactory.GetWrapper <ProductItem, ProductItemWrapper> (pr1);
-            ProductItemWrapper pr2W = WrappersFactory.GetWrapper <ProductItem, ProductItemWrapper> (pr2);
+            var pl1 = new List<ParameterWrapper>(productItemWrapper1.Parameters);
+            var pl2 = new List<ParameterWrapper>(productItemWrapper2.Parameters);
+            pl2.ForEach(productItemWrapper1.Parameters.Add);
+            pl1.ForEach(productItemWrapper2.Parameters.Add);
 
-            Assert.IsFalse(pr1W.HasSameParameters(pr2W));
-
-            var pl1 = new List<ParameterWrapper>(pr1W.Parameters);
-            var pl2 = new List<ParameterWrapper>(pr2W.Parameters);
-            pl2.ForEach(pr1W.Parameters.Add);
-            pl1.ForEach(pr2W.Parameters.Add);
-
-            Assert.IsTrue(pr1W.HasSameParameters(pr2W));
+            Assert.IsTrue(productItemWrapper1.HasSameParameters(productItemWrapper2));
         }
     }
 }
--- HVTApp/HVTApp.Model.Tests/ProjectTests.cs
+++ HVTApp/HVTApp.Model.Tests/ProjectTests.cs
@@ -16,17 +16,17 @@ namespace HVTApp.Model.Tests
             Parameter p1 = new Parameter();
             Parameter p2 = new Parameter();
 
-            Unit su10 = new Unit { ProductionsUnit = new ProductionsUnit { Product = new Product { ProductItem = new ProductItem { Parameters = new List<Parameter>(new[] { p1 }) } } }, SalesUnit = new SalesUnit { Cost = new SumAndVat { Sum = 10, Vat = 1 } } };
-            Unit su11 = new Unit { ProductionsUnit = new ProductionsUnit { Product = new Product { ProductItem = new ProductItem { Parameters = new List<Parameter>(new[] { p1 }) } } }, SalesUnit = new SalesUnit { Cost = new SumAndVat { Sum = 10, Vat = 1 } } };
-            Unit su12 = new Unit { ProductionsUnit = new ProductionsUnit { Product = new Product { ProductItem = new ProductItem { Parameters = new List<Parameter>(new[] { p1 }) } } }, SalesUnit = new SalesUnit { Cost = new SumAndVat { Sum = 10, Vat = 1 } } };
-            Unit su20 = new Unit { ProductionsUnit = new ProductionsUnit { Product = new Product { ProductItem = new ProductItem { Parameters = new List<Parameter>(new[] { p2 }) } } }, SalesUnit = new SalesUnit { Cost = new SumAndVat { Sum = 10, Vat = 1 } } };
-            Unit su21 = new Unit { ProductionsUnit = new ProductionsUnit { Product = new Product { ProductItem = new ProductItem { Parameters = new List<Parameter>(new[] { p2 }) } } }, SalesUnit = new SalesUnit { Cost = new SumAndVat { Sum = 10, Vat = 1 } } };
+            Unit u10 = new Unit { ProductionsUnit = new ProductionsUnit { Product = new Product { ProductItem = new ProductItem { Parameters = new List<Parameter>(new[] { p1 }) } } }, SalesUnit = new SalesUnit { Cost = new SumAndVat { Sum = 10, Vat = 1 } } };
+            Unit u11 = new Unit { ProductionsUnit = new ProductionsUnit { Product = new Product { ProductItem = new ProductItem { Parameters = new List<Parameter>(new[] { p1 }) } } }, SalesUnit = new SalesUnit { Cost = new SumAndVat { Sum = 10, Vat = 1 } } };
+            Unit u12 = new Unit { ProductionsUnit = new ProductionsUnit { Product = new Product { ProductItem = new ProductItem { Parameters = new List<Parameter>(new[] { p1 }) } } }, SalesUnit = new SalesUnit { Cost = new SumAndVat { Sum = 10, Vat = 1 } } };
+            Unit u20 = new Unit { ProductionsUnit = new ProductionsUnit { Product = new Product { ProductItem = new ProductItem { Parameters = new List<Parameter>(new[] { p2 }) } } }, SalesUnit = new SalesUnit { Cost = new SumAndVat { Sum = 10, Vat = 1 } } };
+            Unit u21 = new Unit { ProductionsUnit = new ProductionsUnit { Product = new Product { ProductItem = new ProductItem { Parameters = new List<Parameter>(new[] { p2 }) } } }, SalesUnit = new SalesUnit { Cost = new SumAndVat { Sum = 10, Vat = 1 } } };
 
-            Project project = new Project {Units = new List<Unit>(new[] {su10, su11, su12, su20, su21})};
+            Project project = new Project {Units = new List<Unit>(new[] {u10, u11, u12, u20, u21})};
             ProjectWrapper projectWrapper = WrappersFactory.GetWrapper <Project, ProjectWrapper> (project);
 
             Assert.AreEqual(projectWrapper.UnitsGroups.Count, 2);
-            Assert.AreEqual(projectWrapper.UnitsGroups.First().Units.Count, 3);
+            Assert.AreEqual(projectWrapper.UnitsGroups.First().Count, 3);
         }
     }
 }
