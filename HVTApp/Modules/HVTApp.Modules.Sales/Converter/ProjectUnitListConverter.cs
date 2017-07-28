1f87f5e9b5ea91a850bc463f7e5d08bd10f7f5ae
ALex Kos <kosolapov.ag@gmail.com>
2017-07-28 17:59:09 +0500

    lkj

--- HVTApp/Modules/HVTApp.Modules.Sales/Converter/ProjectUnitListConverter.cs
+++ HVTApp/Modules/HVTApp.Modules.Sales/Converter/ProjectUnitListConverter.cs
@@ -8,15 +8,29 @@ using System.Threading.Tasks;
 using System.Windows.Data;
 using HVTApp.Model.POCOs;
 using HVTApp.Model.Wrappers;
+using Microsoft.Practices.ObjectBuilder2;
 
 namespace HVTApp.Modules.Sales.Converter
 {
-    [ValueConversion(typeof(IList<ProjectUnitWrapper>), typeof(ObservableCollection<ProductsUnitGroup>))]
+    [ValueConversion(typeof(IList<ProjectUnitWrapper>), typeof(ObservableCollection<ProjectUnitGroup>))]
     public class ProjectUnitListConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            ObservableCollection<ProjectUnitGroup> result = new ObservableCollection<ProjectUnitGroup>();
+
+            var units = value as IList<ProductionUnitWrapper>;
+            while (units.Any())
+            {
+                var firstMember = units.First();
+                var members = units.Where(x => x.Product == firstMember.Product);
+
+                result.Add(new ProjectUnitGroup {Product = firstMember.Product.Model, Count = members.Count()});
+
+                members.ForEach(x => units.Remove(x));
+            }
+
+            return result;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -27,6 +41,7 @@ namespace HVTApp.Modules.Sales.Converter
 
     public class ProjectUnitGroup
     {
-        
+        public Product Product { get; set; }
+        public int Count { get; set; }
     }
 }
