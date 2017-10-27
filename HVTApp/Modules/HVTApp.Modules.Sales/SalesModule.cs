91976a0842b5730cdabfb7688a9c0249333f0625
ALex Kos <kosolapov.ag@gmail.com>
2017-10-27 15:21:01 +0500

    27.10.17 - 6

--- HVTApp/Modules/HVTApp.Modules.Sales/SalesModule.cs
+++ HVTApp/Modules/HVTApp.Modules.Sales/SalesModule.cs
@@ -1,22 +1,10 @@
-﻿using Prism.Modularity;
-using Prism.Regions;
-using System;
+﻿using Prism.Regions;
 using HVTApp.Infrastructure;
 using HVTApp.Infrastructure.Interfaces.Services.DialogService;
 using HVTApp.Infrastructure.Prism;
 using HVTApp.Modules.Sales.Menus;
-using HVTApp.Modules.Sales.ViewModels;
 using HVTApp.Modules.Sales.Views;
-using HVTApp.UI.ViewModels;
 using Microsoft.Practices.Unity;
-using ContractsView = HVTApp.UI.Views.ContractsView;
-using OfferDetailsWindow = HVTApp.UI.Views.OfferDetailsWindow;
-using OffersView = HVTApp.UI.Views.OffersView;
-using PaymentsView = HVTApp.UI.Views.PaymentsView;
-using ProductUnitsDetailsWindow = HVTApp.UI.Views.ProductUnitsDetailsWindow;
-using ProjectDetailsWindow = HVTApp.UI.Views.ProjectDetailsWindow;
-using ProjectsView = HVTApp.UI.Views.ProjectsView;
-using TendersView = HVTApp.UI.Views.TendersView;
 
 namespace HVTApp.Modules.Sales
 {
--- HVTApp/Modules/HVTApp.Modules.Sales/ViewModels/MarketViewModel.cs
+++ HVTApp/Modules/HVTApp.Modules.Sales/ViewModels/MarketViewModel.cs
@@ -1,6 +1,40 @@
-﻿namespace HVTApp.Modules.Sales.ViewModels
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using HVTApp.UI.Lookup;
+using Prism.Commands;
+
+namespace HVTApp.Modules.Sales.ViewModels
 {
     public class MarketViewModel
     {
+        private readonly IProjectLookupDataService _projectLookupDataService;
+
+        private bool _loadedFlag = false;
+
+        public MarketViewModel(IProjectLookupDataService projectLookupDataService)
+        {
+            _projectLookupDataService = projectLookupDataService;
+
+            Projects = new ObservableCollection<ProjectLookup>();
+
+            LoadedCommand = new DelegateCommand(async () =>
+            {
+                if(!_loadedFlag) await LoadAsync();
+                _loadedFlag = true;
+            });
+        }
+
+        public ICommand LoadedCommand { get; }
+
+        public ObservableCollection<ProjectLookup> Projects { get; }
+
+        public ProjectLookup SelectedProject { get; set; }
+
+        private async Task LoadAsync()
+        {
+            var lookups = await _projectLookupDataService.GetAllLookupsAsync();
+            Projects.AddRange(lookups);
+        }
     }
 }
