314c9035134450ccf8bf5eed9518f5cff0b069f0
kosolapov (house) <kosolapov.ag@gmail.com>
2017-11-04 01:45:00 +0500

    04.11.07 - 1

---
+++ HVTApp/Modules/HVTApp.Modules.CommonEntities/Controls/ViewBaseList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HVTApp.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for ViewBaseList
+    /// </summary>
+    public partial class ViewBaseList : ViewBase
+    {
+        public ViewBaseList()
+        {
+            InitializeComponent();
+        }
+    }
+}
