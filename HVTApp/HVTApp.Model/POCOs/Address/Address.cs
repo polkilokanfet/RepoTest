604d1c3068ca554f04e73941922cca0afaa9595a
ALex Kos <kosolapov.ag@gmail.com>
2017-11-08 17:06:54 +0500

    123

---
+++ HVTApp/HVTApp.Model/POCOs/Address/Address.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using HVTApp.Infrastructure;
+
+namespace HVTApp.Model.POCOs
+{
+    public class Address : BaseEntity
+    {
+        public string Description { get; set; }
+        public virtual Locality Locality { get; set; }
+
+        public override string ToString()
+        {
+            return Locality.ToString() + "; " + Description;
+        }
+    }
+}
