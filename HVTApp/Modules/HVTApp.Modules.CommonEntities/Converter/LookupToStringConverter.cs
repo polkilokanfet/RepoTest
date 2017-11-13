09ac1ce8343cc7e19b7cbda868ee8bdd521fc644
kosolapov (house) <kosolapov.ag@gmail.com>
2017-11-14 00:10:49 +0500

    465

--- HVTApp/Modules/HVTApp.Modules.CommonEntities/Converter/LookupToStringConverter.cs
+++ HVTApp/Modules/HVTApp.Modules.CommonEntities/Converter/LookupToStringConverter.cs
@@ -17,6 +17,7 @@ namespace HVTApp.UI.Converter
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
+
         }
     }
 }
