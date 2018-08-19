3d935456de06f9702d3e80f955855182890a293b
kosolapov (house) <kosolapov.ag@gmail.com>
2018-08-19 23:27:19 +0500

    19.08

--- HVTApp/HVTApp.Modules.BaseEntities/Menus/BaseEntitiesMenuViewModel.cs
+++ HVTApp/HVTApp.Modules.BaseEntities/Menus/BaseEntitiesMenuViewModel.cs
@@ -9,36 +9,10 @@ namespace HVTApp.Modules.BaseEntities.Menus
     {
         protected override void GenerateMenu()
         {
-            var rootFacility = new NavigationItem("Объекты", typeof(FacilityLookupListView));
-            rootFacility.Items.Add(new NavigationItem("Тип объекта", typeof(FacilityTypeLookupListView)));
+            var views = typeof(ContractLookupListView).Assembly.GetTypes()
+                .Where(x => String.Equals(x.Namespace, typeof(CompanyLookupListView).Namespace) && 
+                !x.Name.Contains("Details") && x.Name.Contains("ListView")).OrderBy(x => x.Name);
 
-            var rootCompany = new NavigationItem("Компании", typeof(CompanyLookupListView));
-            rootCompany.Items.Add(new NavigationItem("Организационные формы", typeof(CompanyFormLookupListView)));
-            rootCompany.Items.Add(new NavigationItem("Сферы деятельности", typeof(ActivityFieldLookupListView)));
-
-            var rootParameter = new NavigationItem("Параметры", typeof(ParameterLookupListView));
-            rootParameter.Items.Add(new NavigationItem("Группа параметров", typeof(ParameterGroupLookupListView)));
-
-            var rootProduct = new NavigationItem("Изделия", typeof(ProductLookupListView));
-            rootProduct.Items.Add(new NavigationItem("Блоки", typeof(ProductBlockLookupListView)));
-
-            //var rootPartPrices = new NavigationItem("Себестоимости", typeof(PartPriceLookupListView));
-            //rootPartPrices.Items.Add(new NavigationItem("Описание блока", typeof(DescribeProductBlockTaskLookupListView)));
-            //rootPartPrices.Items.Add(new NavigationItem("Задание на расчет себеистоимости блока", typeof(CalculatePriceTaskLookupListView)));
-
-            var rootContracts = new NavigationItem("Контракты", typeof(ContractLookupListView));
-
-            Items.Add(rootFacility);
-            Items.Add(rootCompany);
-            Items.Add(rootParameter);
-            Items.Add(rootProduct);
-            //Items.Add(rootPartPrices);
-            Items.Add(rootContracts);
-
-            var uiAssembly = typeof(ContractLookupListView).Assembly;
-            var views = uiAssembly.GetTypes().Where(x => String.Equals(x.Namespace, "HVTApp.UI.Views") && 
-                                                         !x.Name.Contains("Details") &&
-                                                         x.Name.Contains("View")).OrderBy(x => x.Name);
             foreach (var view in views)
             {
                 Items.Add(new NavigationItem(view.Name, view));
