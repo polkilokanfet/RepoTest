2733aacdc0b42453aa2ad904fcf92ea0cd1cfa89
kosolapov (house) <kosolapov.ag@gmail.com>
2018-08-19 16:18:24 +0500

    beforeGenerationOfXaml

--- HVTApp/HVTApp.UI/GeneratorHelpers.cs
+++ HVTApp/HVTApp.UI/GeneratorHelpers.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using HVTApp.Infrastructure;
 using HVTApp.Model.POCOs;
+using HVTApp.UI.Views;
 
 namespace HVTApp.UI
 {
     public static class GeneratorHelpers
     {
+
+        //сохранение сгенерированного кода в отдельный файл.
+        public static void SaveGeneratedCodeAsFile(string textToWrite, string host, string fileName = "ccc.txt", string filePath = "ddd")
+        {
+            string directoryName = Path.GetDirectoryName(host);
+
+            string dir = Path.Combine(directoryName, filePath);
+
+            string outputFilePath = Path.Combine(dir, fileName);
+
+            File.WriteAllText(outputFilePath, textToWrite);
+
+            //GenerationEnvironment.Clear();
+        }
+
+
         /// <summary>
         /// Все типы для генерации окон с деталями.
         /// </summary>
@@ -52,9 +70,10 @@ namespace HVTApp.UI
 
         #region Prop Types
 
-        private static IEnumerable<PropertyInfo> GetPropsWithoutId(Type type)
+        private static IEnumerable<PropertyInfo> GetProps(Type type)
         {
-            return type.GetProperties().Where(x => !Equals(x.Name, "Id"));
+            //return type.GetProperties().Where(x => !Equals(x.Name, "Id"));
+            return type.GetProperties();
         }
 
         /// <summary>
@@ -64,7 +83,7 @@ namespace HVTApp.UI
         /// <returns></returns>
         public static IEnumerable<PropertyInfo> StringProperties(this Type type)
         {
-            return GetPropsWithoutId(type).Where(p => p.PropertyType == typeof(string));
+            return GetProps(type).Where(p => p.PropertyType == typeof(string));
         }
 
         /// <summary>
@@ -76,19 +95,19 @@ namespace HVTApp.UI
         public static IEnumerable<PropertyInfo> SimpleProperties<T>(this Type type)
             where T : struct
         {
-            return GetPropsWithoutId(type).Where(p => p.PropertyType == typeof(T) || p.PropertyType == typeof(T?));
+            return GetProps(type).Where(p => p.PropertyType == typeof(T) || p.PropertyType == typeof(T?));
         }
 
         public static IEnumerable<PropertyInfo> AllSimpleProperties(this Type type)
         {
-            //return GetPropsWithoutId(type).Where(p => p.PropertyType.IsSimple()).Except(type.SimpleProperties<double>()).Except(type.SimpleProperties<DateTime>());
-            return GetPropsWithoutId(type).Where(p => p.PropertyType.IsSimple());
+            //return GetProps(type).Where(p => p.PropertyType.IsSimple()).Except(type.SimpleProperties<double>()).Except(type.SimpleProperties<DateTime>());
+            return GetProps(type).Where(p => p.PropertyType.IsSimple());
         }
 
 
         public static IEnumerable<PropertyInfo> AllCollectionProperties(this Type type)
         {
-            return GetPropsWithoutId(type).Except(type.AllSimpleProperties())
+            return GetProps(type).Except(type.AllSimpleProperties())
                     .Where(p => p.PropertyType.GetInterfaces().Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(ICollection<>)));
         }
 
@@ -100,7 +119,7 @@ namespace HVTApp.UI
         public static IEnumerable<PropertyInfo> AllComplexProperties(this Type type)
         {
             //var allComplexProperties = allProperties.Except(simpleProperties).Where(p => p.PropertyType.IsClass && !typeof(IEnumerable).IsAssignableFrom(p.PropertyType));
-            return GetPropsWithoutId(type).Except(type.AllSimpleProperties()).Except(type.AllCollectionProperties()).Except(type.SimpleProperties<double>()).Except(type.SimpleProperties<DateTime>());
+            return GetProps(type).Except(type.AllSimpleProperties()).Except(type.AllCollectionProperties()).Except(type.SimpleProperties<double>()).Except(type.SimpleProperties<DateTime>());
         }
 
         //public static IEnumerable<PropertyInfo> MyClass(this Type type)
