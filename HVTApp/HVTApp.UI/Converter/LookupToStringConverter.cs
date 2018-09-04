3587a9030ba3fc30437f75278facfcbb276d859c
kosolapov (house) <kosolapov.ag@gmail.com>
2018-09-04 20:07:05 +0500

    5

--- HVTApp/HVTApp.UI/Converter/LookupToStringConverter.cs
+++ HVTApp/HVTApp.UI/Converter/LookupToStringConverter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
 using System.Windows.Data;
 using HVTApp.Infrastructure;
 
@@ -12,7 +14,26 @@ namespace HVTApp.UI.Converter
         {
             var lookup = value as ILookupItem;
             if (lookup == null) return String.Empty;
-            return String.IsNullOrEmpty(lookup.DisplayMember) ? lookup.ToString() : ((ILookupItem) value).DisplayMember;
+            return String.IsNullOrEmpty(lookup.DisplayMember) ? lookup.ToString() : lookup.DisplayMember;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [ValueConversion(typeof(IEnumerable<ILookupItem>), typeof(string))]
+    public class LookupsToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var lookups = value as IEnumerable<ILookupItem>;
+            if (lookups == null) return String.Empty;
+            var builder = new StringBuilder();
+            foreach (var lookup in lookups)
+                builder.Append($"{lookup}; ");
+            return builder.ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
