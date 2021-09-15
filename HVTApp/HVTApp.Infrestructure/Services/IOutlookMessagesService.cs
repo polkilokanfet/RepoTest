fc8decbbbbde0bbfa30451afec9a4312e051d219
kosolapov (house) <kosolapov.ag@gmail.com>
2021-09-15 22:50:17 +0500

    hz

---
+++ HVTApp/HVTApp.Infrestructure/Services/IOutlookMessagesService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace HVTApp.Infrastructure.Services
+{
+    public interface IOutlookMessagesService
+    {
+        IEnumerable<OutlookMessage> GetOutlookMessages(string path);
+    }
+
+    public class OutlookMessage
+    {
+
+    }
+}
---
+++ HVTApp/HVTApp.Services.OutlookMessagesService/OutlookMessagesService1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using HVTApp.Infrastructure.Services;
+
+namespace HVTApp.Services.OutlookMessagesService
+{
+    public class OutlookMessagesService1 : IOutlookMessagesService
+    {
+        public IEnumerable<OutlookMessage> GetOutlookMessages(string path)
+        {
+            using (var msg = new MsgReader.Outlook.Storage.Message(@"C:\test\test.msg"))
+            {
+                var from = msg.Sender;
+                var sentOn = msg.SentOn;
+                var recipientsTo = msg.GetEmailRecipients(MsgReader.Outlook.RecipientType.To, false, false);
+                var recipientsCc = msg.GetEmailRecipients(MsgReader.Outlook.RecipientType.Cc, false, false);
+                var subject = msg.Subject;
+                var htmlBody = msg.BodyHtml;
+                // etc...
+            }
+
+            return new List<OutlookMessage>();
+        }
+    }
+}
