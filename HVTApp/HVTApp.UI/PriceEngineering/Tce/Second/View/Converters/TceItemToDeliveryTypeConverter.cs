2fb0f71a889576a572c66d132cdc855fb41fb9cd
ALex Kos <kosolapov.ag@gmail.com>
2022-06-22 15:47:54 +0500

    ToDeliveryTypeConverter

---
+++ HVTApp/HVTApp.UI/PriceEngineering/Tce/Second/View/Converters/TceItemToDeliveryTypeConverter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace HVTApp.UI.PriceEngineering.Tce.Second.View.Converters
+{
+    [ValueConversion(typeof(TasksTceItem), typeof(string))]
+    public class TceItemToDeliveryTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TasksTceItem tasksTceItem)
+            {
+                var deliveryCost = tasksTceItem.Model.SalesUnits.First().CostDelivery;
+                if (deliveryCost > 0)
+                {
+                    return "Доставка";
+                }
+
+                return "Самовывоз";
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HVTApp/HVTApp.UI/PriceEngineering/Tce/Second/View/Converters/TceItemToRealizationDateConverter.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace HVTApp.UI.PriceEngineering.Tce.Second.View.Converters
+{
+    [ValueConversion(typeof(TasksTceItem), typeof(string))]
+    public class TceItemToRealizationDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TasksTceItem tasksTceItem)
+            {
+                return tasksTceItem.Model.SalesUnits.First().RealizationDateCalculated.ToShortDateString();
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
