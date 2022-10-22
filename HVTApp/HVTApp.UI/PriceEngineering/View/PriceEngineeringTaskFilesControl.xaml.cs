73e921c96e0fc043e73e58ad16d757633f26b660
kosolapov (house) <kosolapov.ag@gmail.com>
2022-10-22 20:16:03 +0500

    PriceEngineeringTaskFilesControl

---
+++ HVTApp/HVTApp.UI/PriceEngineering/View/PriceEngineeringTaskFilesControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HVTApp.UI.PriceEngineering.View
+{
+    public partial class PriceEngineeringTaskFilesControl : UserControl
+    {
+        public static readonly DependencyProperty PriceEngineeringTaskViewModelProperty = DependencyProperty.Register(
+            "PriceEngineeringTaskViewModel", typeof(PriceEngineeringTaskViewModel), typeof(PriceEngineeringTaskFilesControl), new PropertyMetadata(default(PriceEngineeringTaskViewModel)));
+
+        public PriceEngineeringTaskViewModel PriceEngineeringTaskViewModel
+        {
+            get => (PriceEngineeringTaskViewModel)GetValue(PriceEngineeringTaskViewModelProperty);
+            set => SetValue(PriceEngineeringTaskViewModelProperty, value);
+        }
+
+        public PriceEngineeringTaskFilesControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
