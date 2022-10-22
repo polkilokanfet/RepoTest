5b4efc7a7a38559b8091e6fe01a4a1cf180a4fd6
kosolapov (house) <kosolapov.ag@gmail.com>
2022-10-22 19:05:27 +0500

    PriceEngineeringTaskChatControl

---
+++ HVTApp/HVTApp.UI/PriceEngineering/View/PriceEngineeringTaskChatControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HVTApp.UI.PriceEngineering.View
+{
+    public partial class PriceEngineeringTaskChatControl : UserControl
+    {
+        public static readonly DependencyProperty PriceEngineeringTaskViewModelProperty = DependencyProperty.Register(
+            "PriceEngineeringTaskViewModel", typeof(PriceEngineeringTaskViewModel), typeof(PriceEngineeringTaskChatControl), new PropertyMetadata(default(PriceEngineeringTaskViewModel)));
+
+        public PriceEngineeringTaskViewModel PriceEngineeringTaskViewModel
+        {
+            get => (PriceEngineeringTaskViewModel) GetValue(PriceEngineeringTaskViewModelProperty);
+            set => SetValue(PriceEngineeringTaskViewModelProperty, value);
+        }
+
+        public PriceEngineeringTaskChatControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
