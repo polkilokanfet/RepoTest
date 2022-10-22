8588a515a57fbca13c559c8898e6e4a3956c8010
kosolapov (house) <kosolapov.ag@gmail.com>
2022-10-22 18:29:21 +0500

    BlockAddedControl

--- HVTApp/HVTApp.UI/PriceEngineering/View/BlockAddedControl.xaml.cs
+++ HVTApp/HVTApp.UI/PriceEngineering/View/BlockAddedControl.xaml.cs
@@ -1,9 +1,18 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace HVTApp.UI.PriceEngineering.View
 {
     public partial class BlockAddedControl : UserControl
     {
+        public static readonly DependencyProperty ProductBlockAddedProperty = DependencyProperty.Register(
+            "ProductBlockAdded", typeof(PriceEngineeringTaskProductBlockAddedWrapper1), typeof(BlockAddedControl), new PropertyMetadata(default(PriceEngineeringTaskProductBlockAddedWrapper1)));
+
+        public PriceEngineeringTaskProductBlockAddedWrapper1 ProductBlockAdded
+        {
+            get => (PriceEngineeringTaskProductBlockAddedWrapper1) GetValue(ProductBlockAddedProperty);
+            set => SetValue(ProductBlockAddedProperty, value);
+        }
         public BlockAddedControl()
         {
             InitializeComponent();
