1c4296e1b0f8a8e11df7488ccfbf1ad04eb11c45
kosolapov (house) <kosolapov.ag@gmail.com>
2022-10-22 19:22:30 +0500

    PriceEngineeringTaskStatusControl

---
+++ HVTApp/HVTApp.UI/PriceEngineering/View/PriceEngineeringTaskStatusControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using HVTApp.Model.POCOs;
+
+namespace HVTApp.UI.PriceEngineering.View
+{
+    public partial class PriceEngineeringTaskStatusControl : UserControl
+    {
+        public static readonly DependencyProperty StatusEnumProperty = DependencyProperty.Register(
+            "StatusEnum", typeof(PriceEngineeringTaskStatusEnum), typeof(PriceEngineeringTaskStatusControl), new PropertyMetadata(default(PriceEngineeringTaskStatusEnum)));
+
+        public PriceEngineeringTaskStatusEnum StatusEnum
+        {
+            get => (PriceEngineeringTaskStatusEnum) GetValue(StatusEnumProperty);
+            set => SetValue(StatusEnumProperty, value);
+        }
+
+        public PriceEngineeringTaskStatusControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
