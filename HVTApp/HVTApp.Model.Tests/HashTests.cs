f3e4c11a8eb99a96ad9dac1e07128dc9e49c285b
ALex Kos <kosolapov.ag@gmail.com>
2022-11-09 10:59:59 +0500

    Links

---
+++ HVTApp/HVTApp.Model.Tests/HashTests.cs
@@ -0,0 +1,18 @@
+﻿using HVTApp.Model.POCOs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HVTApp.Model.Tests
+{
+    [TestClass]
+    public class HashTests
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            var parameter1 = new Parameter();
+            var parameter2 = new Parameter();
+
+            Assert.AreNotEqual(parameter1, parameter2);
+        }
+    }
+}
