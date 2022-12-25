9df9895373522031e85f03fea1843a3334c66ada
kosolapov (house) <kosolapov.ag@gmail.com>
2022-12-26 01:22:18 +0500

    PriceCalculationsVisibility

---
+++ HVTApp/HVTApp.UI/PriceEngineering/View/Controls/PriceEngineeringTasksControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HVTApp.UI.PriceEngineering.View
+{
+    public partial class PriceEngineeringTasksControl : UserControl
+    {
+        public static readonly DependencyProperty PriceCalculationsVisibilityProperty = DependencyProperty.Register(
+            "PriceCalculationsVisibility", typeof(Visibility), typeof(PriceEngineeringTasksControl), new PropertyMetadata(default(Visibility)));
+
+        public Visibility PriceCalculationsVisibility
+        {
+            get { return (Visibility) GetValue(PriceCalculationsVisibilityProperty); }
+            set { SetValue(PriceCalculationsVisibilityProperty, value); }
+        }
+
+        public PriceEngineeringTasksControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
