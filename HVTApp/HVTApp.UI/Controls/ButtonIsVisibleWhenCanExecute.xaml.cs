65e8a618cdc3f140a4513fb3a031ce239b4f0df1
kosolapov (house) <kosolapov.ag@gmail.com>
2023-03-04 23:19:06 +0500

    ButtonIsVisibleWhenCanExecute

--- HVTApp/HVTApp.UI/Controls/ButtonIsVisibleWhenCanExecute.xaml.cs
+++ HVTApp/HVTApp.UI/Controls/ButtonIsVisibleWhenCanExecute.xaml.cs
@@ -16,20 +16,7 @@ namespace HVTApp.UI.Controls
             get => (ICommandIsVisibleWhenCanExecute) GetValue(CommandProperty);
             set => SetValue(CommandProperty, value);
         }
-
-
-        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
-            "Text", 
-            typeof(string), 
-            typeof(ButtonIsVisibleWhenCanExecute), 
-            new PropertyMetadata(default(string)));
-
-        public string Text
-        {
-            get => (string) GetValue(TextProperty);
-            set => SetValue(TextProperty, value);
-        }
-
+        
         public ButtonIsVisibleWhenCanExecute()
         {
             InitializeComponent();
