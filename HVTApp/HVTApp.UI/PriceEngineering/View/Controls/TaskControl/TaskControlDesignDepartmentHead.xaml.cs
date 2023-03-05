e3c26cb3f095794a8d6a2a144001618811800e85
kosolapov (house) <kosolapov.ag@gmail.com>
2023-03-05 12:16:17 +0500

    TaskControlDesignDepartmentHead

--- HVTApp/HVTApp.UI/PriceEngineering/View/Controls/TaskControl/TaskControlDesignDepartmentHead.xaml.cs
+++ HVTApp/HVTApp.UI/PriceEngineering/View/Controls/TaskControl/TaskControlDesignDepartmentHead.xaml.cs
@@ -5,13 +5,13 @@ namespace HVTApp.UI.PriceEngineering.View
 {
     public partial class TaskControlDesignDepartmentHead : UserControl
     {
-        public static readonly DependencyProperty TaskViewModelProperty = DependencyProperty.Register(
-            "TaskViewModel", typeof(TaskViewModelDesignDepartmentHead), typeof(TaskControlDesignDepartmentHead), new PropertyMetadata(default(TaskViewModelDesignDepartmentHead)));
+        public static readonly DependencyProperty TaskViewModelHeadProperty = DependencyProperty.Register(
+            "TaskViewModelHead", typeof(TaskViewModelDesignDepartmentHead), typeof(TaskControlDesignDepartmentHead), new PropertyMetadata(default(TaskViewModelDesignDepartmentHead)));
 
-        public TaskViewModelDesignDepartmentHead TaskViewModel
+        public TaskViewModelDesignDepartmentHead TaskViewModelHead
         {
-            get { return (TaskViewModelDesignDepartmentHead) GetValue(TaskViewModelProperty); }
-            set { SetValue(TaskViewModelProperty, value); }
+            get { return (TaskViewModelDesignDepartmentHead) GetValue(TaskViewModelHeadProperty); }
+            set { SetValue(TaskViewModelHeadProperty, value); }
         }
 
         public TaskControlDesignDepartmentHead()
