eb592074f8feb2686318762ae4419340d3a02f02
kosolapov (house) <kosolapov.ag@gmail.com>
2023-03-09 20:08:49 +0500

    DoStepCommandProductionRequestFinish

--- HVTApp/HVTApp.UI/PriceEngineering/ViewModel/Task/ViewModel/DoStepCommand/DoStepCommandProductionRequestFinish.cs
+++ HVTApp/HVTApp.UI/PriceEngineering/ViewModel/Task/ViewModel/DoStepCommand/DoStepCommandProductionRequestFinish.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using HVTApp.Model.POCOs;
 using Microsoft.Practices.ObjectBuilder2;
 using Microsoft.Practices.Unity;
@@ -20,6 +21,15 @@ namespace HVTApp.UI.PriceEngineering.DoStepCommand
             var vm = (TaskViewModelBackManager) ViewModel;
             var now = DateTime.Now;
             vm.SalesUnits.ForEach(x => x.SignalToStartProductionDone = now);
+            vm.Messenger.SendMessage($"Открыт з/з {vm.Order.Number}");
+            foreach (var priceEngineeringTask in vm.Model.GetAllPriceEngineeringTasks().Where(x => x.Id != vm.Model.Id))
+            {
+                priceEngineeringTask.Statuses.Add(new PriceEngineeringTaskStatus
+                {
+                    Moment = now, 
+                    StatusEnum = ScriptStep.ProductionRequestFinish.Value
+                });
+            }
             base.DoStepAction();
         }
     }
