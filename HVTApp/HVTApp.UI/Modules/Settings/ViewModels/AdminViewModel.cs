043ba057813e24ec03997d58ff7f556a5a923865
kosolapov (house) <kosolapov.ag@gmail.com>
2023-03-09 20:38:04 +0500

    release

--- HVTApp/HVTApp.UI/Modules/Settings/ViewModels/AdminViewModel.cs
+++ HVTApp/HVTApp.UI/Modules/Settings/ViewModels/AdminViewModel.cs
@@ -46,130 +46,28 @@ namespace HVTApp.UI.Modules.Settings.ViewModels
 
                     var messageService = _container.Resolve<IMessageService>();
                     var unitOfWork = _container.Resolve<IUnitOfWork>();
-                    var engineeringTasks = unitOfWork.Repository<PriceEngineeringTask>().GetAll();
-                    engineeringTasks.ForEach(x => x.IsValidForProduction = true);
-
-                    User backManagerBoss = unitOfWork.Repository<User>().Find(x => x.Employee.Person.Surname == "Игнатенко").First();
-
-                    var priceEngineeringTasks = engineeringTasks
-                        .Where(x => x.ParentPriceEngineeringTaskId == null)
-                        .Where(x => x.SalesUnits.Any())
-                        .Where(x => x.GetPriceEngineeringTasks(unitOfWork).BackManager != null)
-                        .Where(x => x.GetAllPriceEngineeringTasks().All(x1 => x1.Status.Equals(ScriptStep.Accept)))
-                        .ToList();
-
-                    foreach (var task in priceEngineeringTasks)
-                    {
-                        var t = task.GetAllPriceEngineeringTasks();
-                        var tasks = task.GetPriceEngineeringTasks(unitOfWork);
-                        foreach (var tItem in t)
-                        {
-                            var moment = tItem.Statuses.Max(x => x.Moment).AddSeconds(1);
-                            tItem.Statuses.Add(new PriceEngineeringTaskStatus
-                            {
-                                Moment = moment,
-                                StatusEnum = ScriptStep.LoadToTceStart.Value
-                            });
-
-                            tItem.Messages.Add(new PriceEngineeringTaskMessage()
-                            {
-                                Author = backManagerBoss,
-                                Moment = moment.AddSeconds(2),
-                                Message = $"Назначен бэк-менеджер: {tasks.BackManager.Employee.Person}"
-                            });
-
-                            if (string.IsNullOrWhiteSpace(tasks.CommentBackOfficeBoss) == false)
-                            {
-                                tItem.Messages.Add(new PriceEngineeringTaskMessage()
-                                {
-                                    Author = backManagerBoss,
-                                    Moment = moment.AddSeconds(2),
-                                    Message = $"{tasks.CommentBackOfficeBoss}"
-                                });
-                            }
-                        }
-                    }
-
-                    unitOfWork.SaveChanges();
-
-                    foreach (var task in priceEngineeringTasks)
-                    {
-                        var tasks = task.GetPriceEngineeringTasks(unitOfWork);
-
-                        var vm = new TaskViewModelBackManager(new TasksWrapperBackManager(tasks, container), container, task.Id);
-                        if (vm.TasksTceItem.SccVersions.Where(x => x.IsActual).All(x => x.Version.HasValue) == false)
-                        {
-                            continue;
-                        }
-
-                        var t = task.GetAllPriceEngineeringTasks();
-                        var finishMoment = tasks.PriceCalculations
-                            .Where(x => x.TaskOpenMoment.HasValue)
-                            .OrderBy(x => x.TaskOpenMoment.Value)
-                            .LastOrDefault()?
-                            .TaskCloseMoment;
-
-                        foreach (var tItem in t)
-                        {
-                            var moment = tItem.Statuses.Max(x => x.Moment).AddSeconds(1);
-                            var fm = finishMoment.HasValue && finishMoment.Value > moment
-                                ? finishMoment.Value
-                                : moment.AddSeconds(3);
-                            tItem.Statuses.Add(new PriceEngineeringTaskStatus
-                            {
-                                Moment = fm,
-                                StatusEnum = ScriptStep.LoadToTceFinish.Value
-                            });
-                        }
-                    }
-
-                    unitOfWork.SaveChanges();
-
-                    priceEngineeringTasks = unitOfWork.Repository<PriceEngineeringTask>()
+                    var engineeringTasks = unitOfWork.Repository<PriceEngineeringTask>()
                         .Find(x => x.ParentPriceEngineeringTaskId == null)
-                        .Where(x => x.GetAllPriceEngineeringTasks().All(x1 => x1.Status.Equals(ScriptStep.LoadToTceFinish)))
-                        .Where(x => x.SalesUnits.All(s => s.Order != null))
-                        .Where(x => x.SalesUnits.Select(s => s.Order).Distinct().Count() == 1)
-                        .ToList();
+                        .Where(x => x.Status.Equals(ScriptStep.ProductionRequestFinish));
 
-                    foreach (var task in priceEngineeringTasks)
+                    foreach (var task in engineeringTasks)
                     {
-                        var mom = task.Statuses.Max(x => x.Moment).AddSeconds(1);
-                        var dt = task.SalesUnits.First().SignalToStartProduction;
-                        if (dt.HasValue && dt.Value > mom)
-                            mom = dt.Value;
+                        var status = task.Statuses
+                            .OrderBy(x => x.Moment)
+                            .Last(x => x.StatusEnum == ScriptStep.ProductionRequestFinish.Value);
 
-                        task.Statuses.Add(new PriceEngineeringTaskStatus
+                        foreach (var priceEngineeringTask in task.GetAllPriceEngineeringTasks().Where(x => x.Id != task.Id))
                         {
-                            Moment = mom,
-                            StatusEnum = ScriptStep.ProductionRequestStart.Value
-                        });
-
-                        task.Messages.Add(new PriceEngineeringTaskMessage()
-                        {
-                            Author = task.GetPriceEngineeringTasks(unitOfWork).BackManager,
-                            Moment = mom.AddSeconds(1),
-                            Message = $"Открыт з/з: {task.SalesUnits.First().Order.Number}"
-                        });
-
-
-                        dt = task.SalesUnits.First().SignalToStartProductionDone;
-                        if (dt.HasValue && dt.Value > mom)
-                            mom = dt.Value;
-                        else
-                            mom = mom.AddSeconds(2);
-
-                        task.Statuses.Add(new PriceEngineeringTaskStatus
-                        {
-                            Moment = mom,
-                            StatusEnum = ScriptStep.ProductionRequestFinish.Value
-                        });
+                            priceEngineeringTask.Statuses.Add(new PriceEngineeringTaskStatus
+                            {
+                                Moment = status.Moment, StatusEnum = status.StatusEnum
+                            });
+                        }
                     }
 
                     unitOfWork.SaveChanges();
 
                     messageService.ShowOkMessageDialog("", "Finish");
-
 
                     //unitOfWork.SaveChanges();
                     //unitOfWork.Dispose();
