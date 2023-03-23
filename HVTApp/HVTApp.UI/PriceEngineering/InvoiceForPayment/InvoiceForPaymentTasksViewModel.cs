cb191be4d89520f8dc6ed7d9c4b6c45c660ca365
ALex Kos <kosolapov.ag@gmail.com>
2023-03-23 10:56:18 +0500

    CommonInfoUnit

--- HVTApp/HVTApp.UI/PriceEngineering/InvoiceForPayment/InvoiceForPaymentTasksViewModel.cs
+++ HVTApp/HVTApp.UI/PriceEngineering/InvoiceForPayment/InvoiceForPaymentTasksViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using HVTApp.Infrastructure;
 using HVTApp.Infrastructure.Attributes;
+using HVTApp.Infrastructure.Extansions;
 using HVTApp.Infrastructure.Services;
 using HVTApp.Infrastructure.ViewModels;
 using HVTApp.Model;
@@ -80,8 +81,32 @@ namespace HVTApp.UI.PriceEngineering.InvoiceForPayment
                 }
             }
 
+            [Designation("Заказ"), OrderStatus(-1)]
+            public string Order { get; }
+
+            [Designation("Позиции"), OrderStatus(-2)]
+            public string OrderPositions { get; }
+
+            [Designation("Владелец объекта"), OrderStatus(-3)]
+            public string FacilityOwners { get; }
+
+            [Designation("Контрагент"), OrderStatus(-4)]
+            public string Contragent { get; }
+
+            [Designation("Тип контрагента"), OrderStatus(-5)]
+            public string ContragentType { get; }
+
             [Designation("Объект"), OrderStatus(-6)]
             public string Facility { get; }
+
+            [Designation("Страна поставки"), OrderStatus(-1000)]
+            public Country Country { get; }
+
+            [Designation("Федеральный округ"), OrderStatus(-9)]
+            public string District { get; }
+
+            [Designation("Сегмент"), OrderStatus(-10)]
+            public string Segment { get; }
 
             [Designation("Тип продукта"), OrderStatus(-12)]
             public string ProductType { get; }
@@ -110,8 +135,8 @@ namespace HVTApp.UI.PriceEngineering.InvoiceForPayment
             [Designation("Стоимость блоков с фиксированной ценой"), OrderStatus(-26)]
             public double FixedCost { get; }
 
-            [Designation("Контрагент"), OrderStatus(-4)]
-            public string Contragent { get; }
+            [Designation("Менеджер"), OrderStatus(-33)]
+            public string Manager { get; }
 
             [Designation("Договор"), OrderStatus(-34)]
             public string ContractNumber { get; }
@@ -125,6 +150,9 @@ namespace HVTApp.UI.PriceEngineering.InvoiceForPayment
             [Designation("Дата спецификации"), OrderStatus(-37)]
             public DateTime? SpecificationDate { get; }
 
+            [Designation("ОИТ"), OrderStatus(-43)]
+            public DateTime OrderInTakeDate { get; }
+
             [Designation("Условия оплаты"), OrderStatus(-59)]
             public PaymentConditionSet PaymentConditionSet { get; }
 
@@ -134,8 +162,8 @@ namespace HVTApp.UI.PriceEngineering.InvoiceForPayment
             [Designation("Адрес доставки"), OrderStatus(-138)]
             public string DeliveryAddress { get; }
 
-            [Designation("Менеджер"), OrderStatus(-33)]
-            public string Manager { get; }
+            [Designation("Срок производства"), OrderStatus(-245)]
+            public int ProductionTerm { get; }
 
             public Unit(InvoiceForPaymentTask task, IUnitOfWork unitOfWork)
             {
@@ -145,8 +173,28 @@ namespace HVTApp.UI.PriceEngineering.InvoiceForPayment
                 var salesUnits = task.PriceEngineeringTask?.SalesUnits ?? task.TechnicalRequrements.SalesUnits;
                 var salesUnit = salesUnits.First();
 
+                FixedCost = -1.0 * salesUnits
+                    .SelectMany(x => x.ProductsIncluded)
+                    .Where(x => x.Product.ProductBlock.IsSupervision)
+                    .Where(x => x.Product.ProductBlock.FixedCosts.Any())
+                    .Select(x => x.Product.ProductBlock.FixedCosts.Last().Sum)
+                    .Sum();
+
+                Order = salesUnit.Order?.ToString();
+                OrderPositions = salesUnits.Select(unit => unit.OrderPosition).GetOrderPositions();
+
+                ProductionTerm = salesUnit.ProductionTerm;
+                var owners = new List<Company> { salesUnit.Facility.OwnerCompany };
+                owners.AddRange(salesUnit.Facility.OwnerCompany.ParentCompanies().ToList());
+                FacilityOwners = owners.ToStringEnum();
+                var contragent = salesUnit.Specification?.Contract.Contragent;
                 Contragent = salesUnit.Specification?.Contract.Contragent.ToString();
+                ContragentType = GetContragentType(contragent, salesUnits);
                 Facility = salesUnit.Facility.ToString();
+                var region = salesUnit.Facility.GetRegion();
+                Country = region?.District.Country;
+                District = region?.District.Name;
+                Segment = SegmentConverter(GetSegment(salesUnits));
                 ProductType = salesUnit.Product.ProductType.Name;
                 Designation = salesUnit.Product.Designation;
                 Amount = salesUnits.Count;
@@ -154,15 +202,6 @@ namespace HVTApp.UI.PriceEngineering.InvoiceForPayment
                 Cost = salesUnit.Cost;
                 var costDelivery = salesUnits.Select(unit => unit.CostDelivery).Where(x => x.HasValue).Sum(x => x.Value);
                 CostDelivery = -1.0 * costDelivery;
-
-                //var price = GlobalAppProperties.PriceService.GetPrice(salesUnit, salesUnit.OrderInTakeDate, true);
-                var sv = salesUnits
-                    .SelectMany(x => x.ProductsIncluded)
-                    .Where(x => x.Product.ProductBlock.IsSupervision)
-                    .Where(x => x.Product.ProductBlock.FixedCosts.Any())
-                    .Select(x => x.Product.ProductBlock.FixedCosts.Last().Sum)
-                    .Sum();
-                FixedCost = -1.0 * sv;
 
                 Manager = $"{salesUnit.Project.Manager.Employee.Person}";
 
@@ -176,12 +215,71 @@ namespace HVTApp.UI.PriceEngineering.InvoiceForPayment
                     ContractDate = specification.Contract.Date;
                 }
 
+
+                OrderInTakeDate = salesUnit.OrderInTakeDate;
+
                 PaymentConditionSet = salesUnit.PaymentConditionSet;
 
 
                 DeliveryType = Math.Abs(CostDelivery) > 0 ? "Доставка" : "Самовывоз";
 
                 DeliveryAddress = salesUnit.GetDeliveryAddressString();
+            }
+
+            private string GetContragentType(Company contragent, IEnumerable<SalesUnit> salesUnits)
+            {
+                if (Contragent == null)
+                    return "Нет данных";
+
+                var salesUnit = salesUnits.First();
+                if (Equals(salesUnit.Facility.OwnerCompany, contragent) ||
+                    salesUnit.Facility.OwnerCompany.ParentCompanies().Contains(contragent))
+                    return "Конечный заказчик";
+
+                //if (_tenders.FirstOrDefault(x => Equals(x.Winner, contragent)) != null) return "Подрядчик";
+
+                return "Посредник";
+            }
+
+            private string GetSegment(IEnumerable<SalesUnit> salesUnits)
+            {
+                //актуальный список сфер деятельности
+                var actualActivities = new List<ActivityFieldEnum>
+            {
+                ActivityFieldEnum.ElectricityDistribution,
+                ActivityFieldEnum.ElectricityTransmission,
+                ActivityFieldEnum.ElectricityGeneration,
+                ActivityFieldEnum.Fuel,
+                ActivityFieldEnum.RailWay,
+                ActivityFieldEnum.IndustrialEnterprise
+            };
+
+                //сегмент по владельцам объекта
+                var owner = salesUnits.First().Facility.OwnerCompany;
+                do
+                {
+                    var activityField = owner.ActivityFilds.FirstOrDefault(x => actualActivities.Contains(x.ActivityFieldEnum));
+                    if (activityField != null)
+                        return activityField.Name;
+                    owner = owner.ParentCompany;
+                } while (owner != null);
+
+                return "Промышленное предприятие";
+            }
+
+            private string SegmentConverter(string segment)
+            {
+                switch (segment)
+                {
+                    case "Генерация электроэнергии": return "Генерация";
+                    case "Железная дорога": return "РЖД";
+                    case "Передача электроэнергии": return "Сети";
+                    case "Промышленное предприятие": return "Промышленность";
+                    case "Распределение электроэнергии": return "Распределение";
+                    case "Топливно-энергетический сектор": return "Нефтегаз.";
+                }
+
+                return segment;
             }
         }
 
