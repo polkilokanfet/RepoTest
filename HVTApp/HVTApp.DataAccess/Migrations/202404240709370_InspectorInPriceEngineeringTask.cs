5cd2274ad2b92c5f0565062ce1a6431bb71be3dc
ALex Kos <kosolapov.ag@gmail.com>
2024-04-24 12:20:43 +0500

    DBMigration

---
+++ HVTApp/HVTApp.DataAccess/Migrations/202404240709370_InspectorInPriceEngineeringTask.cs
@@ -0,0 +1,22 @@
+﻿namespace HVTApp.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InspectorInPriceEngineeringTask : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.PriceEngineeringTask", "UserConstructorInspector_Id", c => c.Guid());
+            CreateIndex("dbo.PriceEngineeringTask", "UserConstructorInspector_Id");
+            AddForeignKey("dbo.PriceEngineeringTask", "UserConstructorInspector_Id", "dbo.User", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.PriceEngineeringTask", "UserConstructorInspector_Id", "dbo.User");
+            DropIndex("dbo.PriceEngineeringTask", new[] { "UserConstructorInspector_Id" });
+            DropColumn("dbo.PriceEngineeringTask", "UserConstructorInspector_Id");
+        }
+    }
+}
