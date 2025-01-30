e49be91b3965cc869b86df659741e4bb722c1a15
ALex Kos <kosolapov.ag@gmail.com>
2025-01-30 13:47:20 +0500

    CheckPricesCommand

--- HVTApp/HVTApp.UI/ViewModels/List/Custom/ProductBlockLookupListViewModel.cs
+++ HVTApp/HVTApp.UI/ViewModels/List/Custom/ProductBlockLookupListViewModel.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using HVTApp.Infrastructure;
 using HVTApp.Infrastructure.Extensions;
 using HVTApp.Infrastructure.Interfaces.Services.SelectService;
+using HVTApp.Infrastructure.Services;
+using HVTApp.Model;
 using HVTApp.Model.POCOs;
 using HVTApp.UI.Commands;
 using HVTApp.UI.Lookup;
@@ -12,7 +15,32 @@ namespace HVTApp.UI.ViewModels
 {
     public partial class ProductBlockLookupListViewModel
     {
+        private class PricesByQuarter
+        {
+            public int Year { get; }
+            public int Quarter { get; }
+            public int QuarterAbsolute { get; }
+            public List<SumOnDate> SumOnDates { get; }
+
+            public double? Average => SumOnDates.Any()
+                ? SumOnDates.Select(x => x.Sum).Average()
+                : default;
+
+            public PricesByQuarter( IEnumerable<SumOnDate> sumOnDates)
+            {
+                SumOnDates = sumOnDates.ToList();
+                Year = SumOnDates.First().Date.Year;
+                Quarter = SumOnDates.First().Date.Quarter();
+                QuarterAbsolute = SumOnDates.First().Date.QuarterAbsolute();
+            }
+
+            public override string ToString()
+            {
+                return $"{Average:C} на {Quarter} кв. {Year} г.";
+            }
+        }
         public DelegateLogCommand AddParameterCommand { get; private set; }
+        public DelegateLogCommand CheckPricesCommand { get; private set; }
 
         protected override void InitSpecialCommands()
         {
@@ -36,7 +64,80 @@ namespace HVTApp.UI.ViewModels
                 },
                 () => SelectedItems != null && SelectedItems.Any());
 
-            this.SelectedLookupChanged += lookup => AddParameterCommand.RaiseCanExecuteChanged();
+            CheckPricesCommand = new DelegateLogCommand(
+                () =>
+                {
+                    var messageService = Container.Resolve<IMessageService>();
+                    using (var unitOfWork = Container.Resolve<IUnitOfWork>())
+                    {
+                        foreach (var selectedItem in SelectedItems)
+                        {
+                            var productBlock = unitOfWork.Repository<ProductBlock>().GetById(selectedItem.Id);
+                            if (productBlock.Prices.Any() == false) continue;
+
+                            var pricesByQuarterList = productBlock
+                                .Prices
+                                .GroupBy(x => x.Date.QuarterAbsolute())
+                                .Select(x => new PricesByQuarter(x))
+                                .OrderBy(x => x.QuarterAbsolute)
+                                .ToList();
+
+                            var all = pricesByQuarterList
+                                .SelectMany(x => x.SumOnDates)
+                                .OrderBy(x => x.Date)
+                                .ToStringEnum();
+
+                            foreach (var pricesByQuarter in pricesByQuarterList.ToList())
+                            {
+                                //среднее значение в этом квартале
+                                var averageQuarter = pricesByQuarter.Average;
+                                var averageLastQuarter = pricesByQuarterList.FirstOrDefault(x => (x.QuarterAbsolute - 1) == pricesByQuarter.QuarterAbsolute)?.Average;
+                                var averageYear = pricesByQuarterList.Where(x => x.Year == pricesByQuarter.Year).Select(x => x.Average).Average();
+                                var averageLastYear = pricesByQuarterList.Where(x => x.Year - 1 == pricesByQuarter.Year).Select(x => x.Average).Average();
+                                foreach (var sumOnDate in pricesByQuarter.SumOnDates.ToList())
+                                {
+                                    if ((sumOnDate.Sum * 0.5) > averageQuarter ||
+                                        (averageLastQuarter.HasValue && (sumOnDate.Sum * 0.5) > averageLastQuarter) ||
+                                        (sumOnDate.Sum * 0.5) > averageYear ||
+                                        (averageLastYear.HasValue && (sumOnDate.Sum * 0.5) > averageLastYear))
+                                    {
+                                        var sb = new StringBuilder();
+                                        sb.AppendLine(productBlock.Designation);
+                                        sb.AppendLine($"{sumOnDate.Date.Quarter()} квартал {sumOnDate.Date.Year}");
+                                        sb.AppendLine($"подозрительное: {sumOnDate:C}");
+                                        sb.AppendLine($"среднее в текущем году: {averageLastYear:C}");
+                                        sb.AppendLine($"среднее в текущем квартале: {averageQuarter:C}");
+                                        sb.AppendLine($"среднее в прошлом квартале: {averageLastQuarter:C}");
+                                        sb.AppendLine($"среднее по кварталам: {pricesByQuarterList.ToStringEnum()}");
+                                        sb.AppendLine($"все прайсы: {all}");
+                                        sb.AppendLine("Удалить подозрительное значение?");
+                                        var dr = messageService.ConfirmationDialog(sb.ToString());
+                                        if (dr)
+                                        {
+                                            pricesByQuarter.SumOnDates.Remove(sumOnDate);
+                                            if (pricesByQuarter.SumOnDates.Any() == false)
+                                                pricesByQuarterList.Remove(pricesByQuarter);
+                                            productBlock.Prices.Remove(sumOnDate);
+                                            unitOfWork.Repository<SumOnDate>().Delete(sumOnDate);
+                                        }
+                                    }
+                                }
+                            }
+                            
+                        }
+
+                        unitOfWork.SaveChanges();
+                    }
+
+                    messageService.Message("done", "done");
+                },
+                () => SelectedItems != null && SelectedItems.Any());
+
+            this.SelectedLookupChanged += lookup =>
+            {
+                AddParameterCommand.RaiseCanExecuteChanged();
+                CheckPricesCommand.RaiseCanExecuteChanged();
+            };
         }
 
         public override void Load()
